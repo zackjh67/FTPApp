cdc8927af29ac716ec192de067874727562a204c
Zachary Hern <zackjh67@gmail.com>
2017-10-11 22:08:28 -0400

    List command working with multi-threading

--- src/ftp/FTPClient.java
+++ src/ftp/FTPClient.java
@@ -24,7 +24,7 @@ class FTPClient {
         StringTokenizer tokens = new StringTokenizer(sentence);
 
 
-        if (sentence.startsWith("connect")) {
+        if (sentence.toUpperCase().startsWith("CONNECT")) {
             String serverName = tokens.nextToken(); // pass the connect command
             serverName = tokens.nextToken();
             port1 = Integer.parseInt(tokens.nextToken());
@@ -41,24 +41,26 @@ class FTPClient {
 
                 sentence = inFromUser.readLine();
 
-                if (sentence.equals("list:")) {
+                if (sentence.toUpperCase().equals("LIST:")) {
 
-                    port = port + 2;
-                    System.out.println(port);
-                    ServerSocket welcomeData = new ServerSocket(port);
-                    outToServer.writeBytes(port + " " + sentence + " " + '\n');
+                    port1 = port1 + 2;
+                    ServerSocket welcomeData = new ServerSocket(port1);
+                    outToServer.writeBytes(port1 + " " + sentence + " " + '\n');
 
                     Socket dataSocket = welcomeData.accept();
                     DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
                     while (notEnd) {
-                        modifiedSentence = inData.readUTF();
-                        //todo........................................
-                        //todo........................................
+                        modifiedSentence = "" + inData.readUTF();
+                        if(modifiedSentence.equals(" "))
+                            notEnd = false;
+                        //todo remove this print statement i think
+                        System.out.println("modified sentence: " + modifiedSentence);
                     }
-
 
                     welcomeData.close();
                     dataSocket.close();
+                    //make notEnd true again
+                    notEnd = true;
                     System.out.println("\nWhat would you like to do next: \n retr: file.txt ||  				stor: file.txt  || close");
 
                 } else if (sentence.startsWith("retr: ")) {
--- src/ftp/FTPServer.java
+++ src/ftp/FTPServer.java
@@ -6,17 +6,12 @@ import java.util.*;
 //todo.................
 //todo.................
 
-/*********************************************
- * NOTE: THIS IS FIRST BEING IMPLEMENTED W/O MULTITHREADING
- *
- */
 class FTPServer {
     byte[] data;
-
+    int connSockNum = 8415;
 
     public static void main(String argv[]) throws Exception {
 
-        //try {
         //socket outside of while loop for listening
         ServerSocket welcomeSocket = new ServerSocket(12000);
 
@@ -28,110 +23,97 @@ class FTPServer {
             //start thread
             handler.start();
         }
-
-//        } catch (Exception e) {
-//            System.err.println(e);
-//        }
     }
+}
 
     //class for handling individual clients
     class FTPHandler extends Thread {
         private Socket client;
         private Scanner input;
         private PrintWriter output;
+        DataOutputStream outToClient;
+        BufferedReader inFromClient;
+        String fromClient;
+        StringTokenizer tokens;
+        String firstLine;
+        String clientCommand;
+        boolean quit = false;
+        //Socket dataSocket = null;
+        int port;
+        Socket connectionSocket;
 
         public FTPHandler(Socket connectionSocket) throws Exception {
+            this.connectionSocket = connectionSocket;
 
-            DataOutputStream outToClient;
-            BufferedReader inFromClient;
-            String fromClient;
-            StringTokenizer tokens;
-            String firstLine;
-            String clientCommand;
-            boolean quit = false;
-            //Socket dataSocket = null;
-            int port;
 
             outToClient =
                     new DataOutputStream(connectionSocket.getOutputStream());
 
             inFromClient = new BufferedReader(new
                     InputStreamReader(connectionSocket.getInputStream()));
-            while(!quit) {
-                fromClient = inFromClient.readLine();
-
-                tokens = new StringTokenizer(fromClient);
-
-                firstLine = tokens.nextToken();
-                port = Integer.parseInt(firstLine);
-                clientCommand = tokens.nextToken();
-
-                //handle each different command here
-                if (clientCommand.toUpperCase().equals("LIST:")) {
-                    listCommand(connectionSocket, port);
-                }
-                if (clientCommand.toUpperCase().equals("RETR:")) {
-                    retrCommand(connectionSocket, port);
-                }
-                if (clientCommand.toUpperCase().equals("STOR:")) {
-                    storCommand(connectionSocket, port);
-                }
-                if (clientCommand.toUpperCase().equals("QUIT:")) {
-                    quitCommand(connectionSocket, port);
-                }
-            }
-
-
-
-
-            //below is original clienthandler code/////////////////////////////////////////////////////
-            //Set up reference to associated socket...
-            client = connectionSocket;
-
-            try {
-                input = new Scanner(client.getInputStream());
-                output = new PrintWriter(
-                        client.getOutputStream(), true);
-            } catch (IOException ioEx) {
-                ioEx.printStackTrace();
-            }
         }
 
-        @Override
         public void run() {
-            String received;
-            do {
-                //Accept message from client on
-                //the socket's input stream...
-                received = input.nextLine();
+                do {
+                    try {
+                        System.out.println("under try");
+                        fromClient = inFromClient.readLine();
+                        System.out.println("fromClient: " + fromClient);
 
-                //Echo message back to client on
-                //the socket's output stream...
-                output.println("ECHO: " + received);
 
-                //Repeat above until 'QUIT' sent by client...
-            } while (!received.equals("QUIT"));
+                        tokens = new StringTokenizer(fromClient);
+                        //first line is the port number
+                        firstLine = tokens.nextToken();
+                        System.out.println("firstLine: " + firstLine);
+                        port = Integer.parseInt(firstLine);
+                        System.out.println("port: " + port);
+                        //second line is the command
+                        clientCommand = tokens.nextToken();
+                        System.out.println("clientCommand: " + clientCommand);
 
-            try {
-                if (client != null) {
-                    System.out.println(
-                            "Closing down connection...");
-                    client.close();
-                }
-            } catch (IOException ioEx) {
-                System.out.println("Unable to disconnect!");
-            }
+                        //handle each different command here
+                        if (clientCommand.toUpperCase().equals("LIST:")) {
+
+                            listCommand(connectionSocket, port);
+                        }
+                        if (clientCommand.toUpperCase().equals("RETR:")) {
+                            retrCommand(connectionSocket, port);
+                        }
+                        if (clientCommand.toUpperCase().equals("STOR:")) {
+                            storCommand(connectionSocket, port);
+                        }
+                        if (clientCommand.toUpperCase().equals("QUIT:")) {
+                            quitCommand(connectionSocket, port);
+                        }
+                    } catch(Exception e){
+                        System.out.println(e);
+                        //stop loop, might change later
+                        quit = true;
+                    }
+                } while (quit == false);
         }
-    }
 
-    private void listCommand(Socket connectionSocket, int port) throws Exception{
+        private void listCommand(Socket connectionSocket, int port) throws Exception {
         Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
         DataOutputStream dataOutToClient =
                 new DataOutputStream(dataSocket.getOutputStream());
-        //todo..........................
 
+        //read files into file array
+        File folder = new File("./");
+        File[] listOfFiles = folder.listFiles();
+
+        //iterate through each file and print name to output stream
+        for (int i = 0; i < listOfFiles.length; i++) {
+            System.out.println("File " + listOfFiles[i].getName());
+            //can add this if statement  to only recognize files and not dirs
+            //if (listOfFiles[i].isFile()) {
+            dataOutToClient.writeUTF("File " + listOfFiles[i].getName());
+            //}
+        }
+        //todo fix this to real EOF if thats a thing
+            dataOutToClient.writeUTF(" ");
         dataSocket.close();
-        System.out.println("Data Socket closed");
+        System.out.println("List Data Socket closed");
     }
 
     private void retrCommand(Socket connectionSocket, int port)throws Exception{
